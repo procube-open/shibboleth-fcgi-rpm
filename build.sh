a538629ba0a0e8f9196bf898cb754d56985c7bba
Mitsuru Nakakawaji <mitsuru@procube.jp>
2017-08-15 15:24:04 +0900

    add supervisor, fcgi rpm

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir -p ./rpmbuild/{BUILD/x86_64,RPMS,SOURCES,SPECS,SRPMS}
 
 rpmbuild -bb rpmbuild/SPECS/shibboleth.spec -with fastcgi
 yumdownloader --destdir rpmbuild/RPMS/x86_64 libcurl-openssl liblog4shib1 libsaml8 libsaml9 libxml-security-c17 \
-     libxmltooling6 libxmltooling7 opensaml-schemas xmltooling-schemas
+     libxmltooling6 libxmltooling7 opensaml-schemas xmltooling-schemas supervisor fcgi
